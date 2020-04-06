262e61178f3bd4fb2fd11dbc636665c85d70573b
Fabian Oraze <59927639+orazefabian@users.noreply.github.com>
2020-04-06 12:23:42 +0200

    initial commit

---
+++ src/main/java/ab1/impl/Nachnamen/Ab1Impl.java
@@ -0,0 +1,26 @@
+package ab1.impl.Nachnamen;
+
+import ab1.Ab1;
+
+public class Ab1Impl implements Ab1 {
+
+	@Override
+	public int binarySearch(int[] data, int element) {
+		return 0;
+	}
+
+	@Override
+	public void shellSort(Integer[] data) {
+
+	}
+
+	@Override
+	public void quickSortStable(Integer[] data) {
+
+	}
+
+	@Override
+	public int[][] mult(int[][] m1, int[][] m2) {
+		return new int[0][];
+	}
+}
---
+++ src/test/java/ab1/Ab1Test.java
@@ -0,0 +1,11 @@
+package ab1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Ab1Test {
+	@Test
+	public void failBecauseOfMissingTests() {
+		Assertions.fail("Test will follow");
+	}
+}
