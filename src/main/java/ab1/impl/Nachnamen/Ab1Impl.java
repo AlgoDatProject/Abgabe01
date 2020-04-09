5b9760ee2d27b15d4226090265e6517bab179870
SamuelMiklau <63232105+SamuelMiklau@users.noreply.github.com>
2020-04-09 13:24:08 +0200

    Three Integers
    Created three integers for searching in the given Array.

--- src/main/java/ab1/impl/Nachnamen/Ab1Impl.java
+++ src/main/java/ab1/impl/Nachnamen/Ab1Impl.java
@@ -6,6 +6,11 @@ public class Ab1Impl implements Ab1 {
 
 	@Override
 	public int binarySearch(int[] data, int element) {
+ int low = 0;
+ int high = data.length()-1;
+ int mid = 0; 
+
+
 		return 0;
 	}
 
