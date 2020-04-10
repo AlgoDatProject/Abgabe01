823e591a6c07a785df391fa05ba307620d3c8735
SamuelMiklau <63232105+SamuelMiklau@users.noreply.github.com>
2020-04-10 12:20:02 +0200

    Test for BinarySearch
    Implemented two prematurely Test-Cases for BinarySearch.
    1. Case is, that the Element is Found and the Program returns the Index.
    
    2.Case is, that the Element is not found

--- src/test/java/ab1/Ab1Test.java
+++ src/test/java/ab1/Ab1Test.java
@@ -8,4 +8,30 @@ public class Ab1Test {
 	public void failBecauseOfMissingTests() {
 		Assertions.fail("Test will follow");
 	}
+
+@Test
+public void TestCorrectElement(){
+	int arr [] = {1,2,3,4,5,6};
+	int search = 5;
+
+	Sort s = new Sort();
+
+	int erg = s.binarySearch(arr,search);
+
+	Assert.assertEquals(4,erg);
+}
+
+@Test
+public void TestElementNotFound (){
+	int arr [] = {1,2,3,4,5,6};
+	int search = 8;
+
+	Sort s = new Sort();
+
+	int erg = s.binarySearch(arr,search);
+
+	Assert.assertEquals(-1,erg);
+}
+
+
 }
